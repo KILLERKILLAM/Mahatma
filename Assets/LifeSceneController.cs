caa737997d32dbde3aae31a1b669872ca2dbadd3
Uma Maheswari Gorusupudi <gumaheswari16@gmail.com>
2018-11-21 01:20:50 +0530

    added scripts

---
+++ Assets/LifeSceneController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class LifeSceneController : MonoBehaviour {
+
+    public GameObject Image1, Image2, Image3;
+    public TextMesh Text1, Text2, Text3;
+    public List<Sprite> Gandhi;
+    public List<string> Info;
+
+    public Button NextButton;
+    public Button PreviousButton;
+
+
+    public int index = 0;
+
+	// Use this for initialization
+	void Start () {
+
+        PreviousButton.enabled = false;
+        index = 0;
+
+        Image1.gameObject.GetComponent<SpriteRenderer>().sprite = Gandhi[index];
+        Image2.gameObject.GetComponent<SpriteRenderer>().sprite = Gandhi[index + 1];
+        Image3.gameObject.GetComponent<SpriteRenderer>().sprite = Gandhi[index + 2];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+    public void next()
+    {
+        index = index + 3;
+        Image1.gameObject.GetComponent<SpriteRenderer>().sprite = Gandhi[index];
+        Image2.gameObject.GetComponent<SpriteRenderer>().sprite = Gandhi[index + 1];
+        Image3.gameObject.GetComponent<SpriteRenderer>().sprite = Gandhi[index + 2];
+
+        if(index==0)
+        {
+            PreviousButton.enabled = false;
+        }
+        if(index+3==Gandhi.Count)
+        {
+            NextButton.enabled = false;
+        }
+    }
+
+
+    public void Previuos()
+    {
+        index = index - 3;
+        Image1.gameObject.GetComponent<SpriteRenderer>().sprite = Gandhi[index];
+        Image2.gameObject.GetComponent<SpriteRenderer>().sprite = Gandhi[index + 1];
+        Image3.gameObject.GetComponent<SpriteRenderer>().sprite = Gandhi[index + 2];
+
+        if (index == 0)
+        {
+            PreviousButton.enabled = false;
+        }
+        if (index + 3 == Gandhi.Count)
+        {
+            NextButton.enabled = false;
+        }
+    }
+}
